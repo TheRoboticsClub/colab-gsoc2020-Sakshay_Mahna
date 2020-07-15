52a3086c9a87ed3037e07d5064f783b51d86f235
SakshayMahna <sakshum19@gmail.com>
2020-07-15 12:23:31 +0530

    Psuedo-automate installation

---
+++ evolutionary_robots/installation.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# catkin_build the resources
+cd catkin_ws
+catkin_make
+
+# After the resources are built, they are sourced
+source devel/setup.bash
+
+# Set the environment variable to use our Gazebo Models
+export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:$PWD/src/evolutionary_robotics/models/
+
+# Navigate back to where we came from
+cd ..
+
+# The installation would be complete now
+# The user can run the exercise
+
